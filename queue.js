7bc4a9126a45a44f5c35b616d350c63395875bc3
Cella Chung <cellachung@MacBook-Pro.local>
2022-06-12 20:37:48 -0700

    adds formatting for Queue isEmpty method, Stack class completed with enqueue, dequeue, peek, and isEmpty methods--passes all tests

--- queue.js
+++ queue.js
@@ -90,7 +90,6 @@ class Queue {
     } else {
       return false;
     }
-
   }
 }
 
--- stack.js
+++ stack.js
@@ -19,26 +19,49 @@ class Stack {
   /** push(val): add new value to the top of the stack. Returns undefined. */
 
   push(val) {
-
+    let newNode = new Node(val);
+    if(this.size === 0){
+      this.top = newNode;
+      this.size++;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+      this.size++;
+    }
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    if(!this.size){ 
+      //you don't have to return if you're throwing an error
+      //execution of the current fn will stop 
+      throw new Error("This stack is empty, nothing to remove!");
+    } else {
+      let oldTop = this.top;
+      let newTop = oldTop.next;
+      oldTop.next = null;
+      this.top = newTop;
+      this.size--;
+      return oldTop.val;
+    }
   }
 
   /** peek(): return the value of the top node in the stack. */
 
   peek() {
-
+    if(this.size === 0){
+      return new Error("This stack is empty!");
+    } else {
+      return this.top.val;
+    }
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0 ? true : false;
   }
 }
 
